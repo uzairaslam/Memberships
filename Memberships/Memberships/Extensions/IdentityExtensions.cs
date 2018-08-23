f5588badd9f73aa7322e95dc06e21b49e201237b
uzairaslam - Uzair Aslam <uzairaslam64@gmail.com>
2018-08-23 22:33:39 +0500

    Added the GetUsers Extension Method

--- Memberships/Memberships/Extensions/IdentityExtensions.cs
+++ Memberships/Memberships/Extensions/IdentityExtensions.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Security.Principal;
+using System.Threading.Tasks;
 using System.Web;
 using Memberships.Models;
 
@@ -14,6 +16,18 @@ namespace Memberships.Extensions
             var db = ApplicationDbContext.Create();
             var user = db.Users.FirstOrDefault(u => u.UserName.Equals(identity.Name));
             return user != null ? user.FirstName : String.Empty;
+        }
+
+        public static async Task GetUsers(this List<UserViewModel> users)
+        {
+            var db = ApplicationDbContext.Create();
+            users.AddRange(await (from u in db.Users
+                    select new UserViewModel
+                    {
+                        Id = u.Id,
+                        Email = u.Email,
+                        FirstName = u.FirstName
+                    }).OrderBy(o => o.Email).ToListAsync());
         }
     }
 }
