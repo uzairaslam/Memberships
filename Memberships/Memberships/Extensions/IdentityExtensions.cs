270560d9d75627f30b106660b8e4fa25d19c3013
uzairaslam - Uzair Aslam <uzairaslam64@gmail.com>
2018-08-23 22:26:29 +0500

    Added GetuserFirstName Extension Method

---
+++ Memberships/Memberships/Extensions/IdentityExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using Memberships.Models;
+
+namespace Memberships.Extensions
+{
+    public static class IdentityExtensions
+    {
+        public static string GetUserFirstName(this IIdentity identity)
+        {
+            var db = ApplicationDbContext.Create();
+            var user = db.Users.FirstOrDefault(u => u.UserName.Equals(identity.Name));
+            return user != null ? user.FirstName : String.Empty;
+        }
+    }
+}
