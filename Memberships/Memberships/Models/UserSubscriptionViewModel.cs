9bc0fbbd0d2835776dbd58aaaeaaac35f64cac01
uzairaslam - Uzair Aslam <uzairaslam64@gmail.com>
2018-08-27 22:28:30 +0500

    Added UserSubscriptionModel class

--- Memberships/Memberships/Models/UserSubscriptionViewModel.cs
+++ Memberships/Memberships/Models/UserSubscriptionViewModel.cs
@@ -9,7 +9,7 @@ namespace Memberships.Models
     public class UserSubscriptionViewModel
     {
         public ICollection<Subscription> Subscriptions { get; set; }
-        //public ICollection<UserSubscriptionModel> UserSubscriptions { get; set; }
+        public ICollection<UserSubscriptionModel> UserSubscriptions { get; set; }
         public bool DisableDropDown { get; set; }
         public string UserId { get; set; }
         public int SubscriptionId { get; set; }
